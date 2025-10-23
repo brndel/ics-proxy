da42a0d23ba10d22316ad6006eb0729bbdb74452
Armin <ar.braendle@gmail.com>
2025-10-23 03:20:23 +0200

    fix: add rust-overlay

--- flake.nix
+++ flake.nix
@@ -3,15 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    {
 
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-    module = import ./.;
+      nixpkgs.overlays = [
+        rust-overlay.overlays.default
+      ];
 
-  };
+      module = import ./.;
+
+    };
 }
