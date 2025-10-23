b8a0e892a9799080d8db007e34a4d0ccc0b9817e
Armin <ar.braendle@gmail.com>
2025-10-23 03:26:49 +0200

    fix: actually set pkgs

--- flake.nix
+++ flake.nix
@@ -18,16 +18,12 @@
       ...
     }:
     {
-
-      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-      nixpkgs.overlays = [
-        rust-overlay.overlays.default
-      ];
+      pkgs = import <nixpkgs> {
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+      };
 
       module = import ./.;
-
     };
 }
